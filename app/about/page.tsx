21c7506bf69ce93665fea658c4d4a62f84387b60
Harsh-09A <harsh.autowebbed@gmail.com>
2026-07-07 19:26:01 +0530

    New Team Section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -38,7 +38,7 @@ export default function AboutPage() {
             <About />
             <Process />
             {/* <FoundersMessage/> */}
-            {/* <TeamSection/> */}
+            <TeamSection/>
 
             {/* <Projects /> */}
             <Awards />
